424fab461348f969dd603a25e9e6076cb5925fa9
Dallin Dahl <dallinsquared@gmail.com>
2015-11-05 14:12:24 -0800

    first draft of code

--- whirlytubes/whirlytubes.go
+++ whirlytubes/whirlytubes.go
@@ -1,3 +1,22 @@
 package whirlytubes
 
+import (
+	"net"
+	"regexp"
+)
 
+type Address interface {
+	Send(string) error
+	Receive() (string, error)
+	Verify() (bool, error)
+}
+
+type TcpAddress net.Conn
+
+func (addr TcpAddress) Verify() (b bool, err error) {
+	b, err = regexp.MatchString(".*:.*", addr) //fix connection for address, store connection in address
+	return
+}
+
+func (addr TcpAddress) Send(msg string) (err error) {
+}
