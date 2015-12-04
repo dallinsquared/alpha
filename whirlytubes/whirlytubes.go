080992115588975d66e91ae54b7257d8453f97d3
Dallin Dahl <dallinsquared@gmail.com>
2015-12-04 12:35:55 -0800

    added init function for addresses

--- whirlytubes/whirlytubes.go
+++ whirlytubes/whirlytubes.go
@@ -10,7 +10,8 @@ import (
 type Address interface {
 	Send(string) error
 	Receive() (string, error)
-	Verify() (bool, error)
+	verify() (bool, error)
+	Init(string) error
 }
 
 type TcpAddress struct {
@@ -18,11 +19,23 @@ type TcpAddress struct {
 	Cnxn net.TCPConn
 }
 
-func (addr TcpAddress) Verify() (b bool, err error) {
+func (addr TcpAddress) verify() (b bool, err error) {
 	b, err = regexp.MatchString(".*:.*", addr.Addr) //fix connection for address, store connection in address
 	return
 }
 
+func (addr TcpAddress) Init(a string) error {
+	addr.Addr = a
+	b, err := addr.verify()
+	if err != nil {
+		return err
+	}
+	if !b {
+		addr.Addr = nil
+		return fmt.Errorf("Address Initialization: malformed address: %s\n\tshould be I.P.add.ress:port\n", a)
+	}
+	return nil
+}
 func (addr TcpAddress) Send(msg string) (err error) {
 	conn, err := net.Dial("tcp", addr.Addr)
 	if err != nil {
